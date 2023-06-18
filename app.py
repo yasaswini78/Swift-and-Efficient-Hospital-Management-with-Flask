0cd32a4418e4ec2a8e6af908fbbb3870d407b65c
yasaswini78 <120036803+yasaswini78@users.noreply.github.com>
2023-06-18 16:45:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for,session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
+from key import secret_key
 import re
 
 app=Flask(__name__)
 
-app.secret_key = 'Your Secret Key'
+app.secret_key = secret_key
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'Your Password'
